17c2c5a00b673aff0150e3956074efbb913d0ee0
ljaramillom <jaramillo.lizet@gmail.com>
2020-08-11 15:02:52 -0500

    add functionality reactive-form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -44,6 +44,7 @@ import { CommonModule } from '@angular/common';
     BrowserModule,
     AppRoutingModule,
     CommonModule,
+    ReactiveFormsModule,
     FormsModule,
     HttpClientModule,
   ],
--- src/app/components/formularios/reactive-form/reactive-form.component.ts
+++ src/app/components/formularios/reactive-form/reactive-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-form',
@@ -8,11 +9,48 @@ import { Component, OnInit } from '@angular/core';
 export class ReactiveFormComponent implements OnInit {
 
   title: string;
+  registerForm: FormGroup;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
-    this.title = 'Formulario reactivo';
+    this.title = 'Registro de usuario';
+    this.createForm();
+  }
+
+  get username() {
+    return this.registerForm.get('username');
+  }
+
+  get password() {
+    return this.registerForm.get('password');
+  }
+
+  createForm() {
+    this.registerForm = this.formBuilder.group({
+      username: ['', {
+        validators: [Validators.required],
+        updateOn: 'blur'
+      }],
+      password: ['', {
+        validators: [Validators.required, Validators.minLength(4)]
+      }]
+    });
+  }
+
+  submit() {
+    if (!this.registerForm.valid) {
+      alert('Por favor verifica la información.');
+      return;
+    }
+    console.log(this.registerForm.value);
+  }
+
+  refrescar() {
+    this.registerForm.patchValue({
+      username: '',
+      password: ''
+    });
   }
 
 }
